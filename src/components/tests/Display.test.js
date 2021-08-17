1002ee7cbafc746639c1c45d1828af08a2923f60
Andrewschaer <andrewschaer@github.com>
2021-08-17 00:05:31 -0400

    Display component tests completed

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,12 +1,72 @@
+import React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Display from './../Display';
+import fetchShow from '../../api/fetchShow'
+jest.mock('../../api/fetchShow')
 
 
+test('renders Display renders without any passed in props', ()=>{
+    // ACT/ARRANGE/ASSERT:
+    render(<Display/>)
+});
+
+test('when button is clicked, gets show data', async ()=>{
+    fetchShow.mockResolvedValueOnce({
+            name: 'Turkey',
+            summary: 'Lunch Meats',
+            seasons: [{id:'1', name: 'Turkey goes hunting', episodes: []}, {id:'2', name: 'Turkey goes ice skating', episodes: []}]      
+    });
+//     // ARRANGE:
+    render(<Display/>)
+//     // ACT:
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    // ASSERT:
+    const showContainer = await screen.findAllByTestId('show-container');
+    expect(showContainer).toHaveLength(1)
+});
+
+const testData = {
+    name: 'Turkey',
+    summary: 'Lunch Meats',
+    seasons: [{id:'1', name: 'Turkey goes hunting', episodes: []}, {id:'2', name: 'Turkey goes ice skating', episodes: []}]      
+}
+
+test('when button is clicked, amount of select options is equal to amount of seasons in test data', async ()=>{
+    fetchShow.mockResolvedValueOnce({
+        name: 'Turkey',
+        summary: 'Lunch Meats',
+        seasons: [{id:'1', name: 'Turkey goes hunting', episodes: []}, {id:'2', name: 'Turkey goes ice skating', episodes: []}]      
+    });
+    // ARRANGE:
+    render(<Display/>)
+    // ACT:
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    // ASSERT:
+    const selectOptions = await screen.findAllByTestId('season-option');
+    expect(selectOptions).toHaveLength(testData.seasons.length);
+});
 
 
-
-
-
-
-
+test('when button is clicked, DisplayFunc function is called', async ()=>{
+    const fakeDisplayFunc = jest.fn();
+    fetchShow.mockResolvedValueOnce({
+        name: 'Turkey',
+        summary: 'Lunch Meats',
+        seasons: [{id:'1', name: 'Turkey goes hunting', episodes: []}, {id:'2', name: 'Turkey goes ice skating', episodes: []}]      
+    });
+    // ARRANGE:
+    render(<Display displayFunc={fakeDisplayFunc}/>)
+    // ACT:
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    // ASSERT
+    const showContainer = await screen.findAllByTestId('show-container');
+    expect(fakeDisplayFunc).toBeCalledTimes(1);
+});
 
 
 
